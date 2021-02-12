c9050995369315b2725d397d81b9694a48951add
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2021-02-12 20:42:58 -0300

    APP CRONOMETRO FUNCIONANDO

---
+++ cronometro/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Index from './src/Index'
+
+export default function App() {
+  return (
+    <Index/>
+  );
+}
